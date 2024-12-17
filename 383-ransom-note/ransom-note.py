9346013d43814c6bb950f7257aa6a9621fc60953
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2024-12-17 16:59:24 +0530

    Time: 57 ms (18.04%) | Memory: 12.7 MB (5.40%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,5 +1,16 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
+        if len(ransomNote)==1 and len( magazine)==1:
+            if  magazine==ransomNote:
+                return True
+            else:
+                return False
+        if len(ransomNote)==1:
+            if ransomNote in magazine:
+                return True
+            else:
+                return False
+        
         mag=Counter( magazine)
         ran=Counter(ransomNote)
         for  key , value in ran.items():

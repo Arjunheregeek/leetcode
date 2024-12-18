17b54fb0de4f3f75a15317cdb062d67387409993
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2024-12-18 18:42:01 +0530

    Time: 14 ms (32.51%) | Memory: 15.1 MB (6.97%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -5,19 +5,18 @@ class Solution(object):
         if len(s) != len(t):
             return False
         
-        # Two dictionaries for bidirectional mapping
         s_to_t = {}
         t_to_s = {}
         
         for char_s, char_t in zip(s, t):
-            # Check consistency in s -> t mapping
+           
             if char_s in s_to_t:
                 if s_to_t[char_s] != char_t:
                     return False
             else:
                 s_to_t[char_s] = char_t
             
-            # Check consistency in t -> s mapping
+            
             if char_t in t_to_s:
                 if t_to_s[char_t] != char_s:
                     return False

776b69f8f420253fb6bf6744790f211b31e1dedb
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2024-12-22 17:43:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (8.64%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -1,8 +1,14 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        sum=0
-        for i in range(1,len(s)):
-            sum = sum+ abs(ord(s[i])-ord(s[i-1]))
-        return sum
+        total_diff = 0
+        for i in range(1, len(s)):
+            total_diff += abs(ord(s[i]) - ord(s[i - 1]))
+        return total_diff
 
-        
+solution = Solution()
+s = "hello"
+result = solution.scoreOfString(s)
+print(result)
+s = "zaz"
+result = solution.scoreOfString(s)
+print(result)

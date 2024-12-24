252fbeb90e7772c605f9ccb2608de1aa52dfdfc5
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2024-12-24 09:15:45 +0530

    Time: 3 ms (99.47%) | Memory: 18 MB (7.75%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,19 +1,14 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        s=boxes
-        ans=[0]*len(s)
-        for i in range(len(s)):
-            sum=0
-
-            for j in range (len(s)):
-                if int(s[j])>0 and j !=i:
-
-                    k=abs(i-j)
-                    sum= sum+k
-                    
-          
-            ans[i]=sum
+        ans = [0]*len(boxes)
+        leftCount, leftCost, rightCount, rightCost, n = 0, 0, 0, 0, len(boxes)
+        for i in range(1, n):
+            if boxes[i-1] == '1': leftCount += 1
+            leftCost += leftCount 
+            
+            ans[i] = leftCost
+        for i in range(n-2, -1, -1):
+            if boxes[i+1] == '1': rightCount += 1
+            rightCost += rightCount
+            ans[i] += rightCost
         return ans
-
-
-        

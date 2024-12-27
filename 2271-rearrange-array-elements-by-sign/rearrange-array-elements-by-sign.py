d697a4f8b00c43c015a4b0346b468dd03097bebc
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2024-12-28 01:08:14 +0530

    Time: 59 ms (36.36%) | Memory: 42.1 MB (36.61%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,6 +1,6 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        a=len(nums)//2
+        '''a=len(nums)//2
         po=[]
         ne=[]
         ans=[]
@@ -12,7 +12,21 @@ class Solution:
         for i in range (a):
             ans.append(po[i])
             ans.append(ne[i])
+        return ans'''
+        
+        ans=[0]*len(nums)
+        p=0
+        n=1
+        for i in range(len(nums)):
+            if nums[i]>=0:
+                ans[p]=nums[i]
+                p+=2
+            else:
+                ans[n]=nums[i]
+                n+=2
         return ans
+
+            
 
 
 

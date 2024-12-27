ff7681bc439abd09965b54bf1860cab7fe5df3f7
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2024-12-27 16:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (6.96%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -1,12 +1,13 @@
+from typing import List
+
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-
+       
         a = len(matrix)
-        rotated = copy.deepcopy(matrix)
-        for  i in range (a):
+        for i in range(a):
+            for j in range(i + 1, a):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-
-            for j in range (a):
-                matrix[j][a-1-i]=rotated[i][j]
         
-        
+        for i in range(a):
+            matrix[i].reverse()

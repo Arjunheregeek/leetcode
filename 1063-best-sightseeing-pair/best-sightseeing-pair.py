28793c5dc3bc4a53076612a699c6ad32a3f6573b
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2024-12-27 15:49:13 +0530

    Time: 58 ms (84.88%) | Memory: 23.3 MB (20.49%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        max_score = 0  # Initialize maximum score
-        max_value = values[0]  # Start with the first value adjusted for i (values[i] + i)
+        max_score = 0 
+        max_value = values[0]  
+        if len(values)==2:
+            return values[0]+values[1]-1
         
+
         for j in range(1, len(values)):
-            # Calculate the score for the pair (i, j)
+            
             max_score = max(max_score, max_value + values[j] - j)
             
-            # Update max_value to consider the best i so far
+           
             max_value = max(max_value, values[j] + j)
         
         return max_score

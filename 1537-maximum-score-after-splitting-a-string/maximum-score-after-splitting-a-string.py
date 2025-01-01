7c49a4aa5e0df935289fa531923ca05d02ab8edc
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-01-01 17:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (9.48%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,10 +1,17 @@
 class Solution:
     def maxScore(self, s: str) -> int:
         n=len(s)
+        ans=0
 
         left = [0] *(n+1)
 
         right = [0] * (n+1)
+        for j in range(len(s) - 2, -1, -1):
+
+            if s[j + 1] == "1":
+                right[j+1] = right[j +2] + 1
+            else:
+                right[j+1]= right[j+2]
 
         for i in range(1, len(s)):
 
@@ -12,17 +19,13 @@ class Solution:
                 left[i] = left[i - 1] + 1
             else:
                 left[i]=left[i-1]
-        
-
-        for j in range(len(s) - 2, -1, -1):
-
-            if s[j + 1] == "1":
-                right[j+1] = right[j +2] + 1
-            else:
-                right[j+1]= right[j+2]
+            ans=max(ans,left[i]+right[i])
+        return ans 
         
 
         
-        for i in range(len(s)):
-            left[i] = left[i] + right[i]
-        return max(left)
+            
+        
+
+        
+       

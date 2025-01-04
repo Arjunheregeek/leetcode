4c273469932302c163891188ef9087e0e83a2bdd
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-01-04 18:00:40 +0530

    Time: 77 ms (96.70%) | Memory: 18.3 MB (22.86%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,32 +1,10 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        # Track the first and last positions of each character
-        first_pos = [-1] * 26
-        last_pos = [-1] * 26
-        result_set = set()
-        
-        # Iterate over the string
-        for i, char in enumerate(s):
-            idx = ord(char) - ord('a')
-            # Update last position for this character
-            last_pos[idx] = i
-            
-        # Iterate backward to fill the first position
-        for i in range(len(s) - 1, -1, -1):
-            idx = ord(s[i]) - ord('a')
-            first_pos[idx] = i
+        res = 0
 
-        # Iterate over each character as the middle of a palindrome
-        for mid_idx, mid_char in enumerate(s):
-            mid = ord(mid_char) - ord('a')
-            
-            # For each character `a` in the alphabet
-            for a in range(26):
-                if first_pos[a] != -1 and first_pos[a] < mid_idx and last_pos[a] > mid_idx:
-                    # Add unique palindrome
-                    result_set.add((chr(a + ord('a')), mid_char, chr(a + ord('a'))))
-        
-        # The number of unique palindromes
-        return len(result_set)
-        
-        
+        for ch in range(26):
+            c = chr(ord('a') + ch)
+            leftIdx, rightIdx = s.find(c), s.rfind(c)
+            if leftIdx != rightIdx:
+                res += len(set(s[leftIdx+1:rightIdx]))
+        return res

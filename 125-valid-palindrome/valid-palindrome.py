caa598180766054419c05b0eeb58186e26326e24
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-01-06 21:05:46 +0530

    Time: 2 ms (99.19%) | Memory: 19 MB (21.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,5 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        k= [x for x in s.lower() if x in "1234567890qwertyuiopasdfghjklmnbvcxz"]
-        return  k== k[::-1]
+        s = [x for x in s.lower() if x in "1234567890qwertyuiopasdfghjklmnbvcxz"]
+        return s == s[::-1]
 

2ea69ecbdd82d303c9a6a052aa7162fd24374ad8
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-01-09 19:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (39.31%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,8 +1,12 @@
 class Solution(object):
-    def rotate(self, nums, k):
-        n=len(nums)
-        ans=[0]*n
-        for i in range (n):
-            ans[(i+k)%n]=nums[i]
-        for i in range (n):
-            nums[i]=ans[i]
+   def rotate(self, nums, k):
+       # Get the actual number of rotations
+       k = k % len(nums)      
+       # Get the number of elements to move from the end to the beginning
+       r = len(nums) - k
+       # Store the elements to move
+       new = nums[0:r]
+       # Remove the elements from the beginning
+       nums[0:r] = []
+       # Append the stored elements to the end
+       nums.extend(new)

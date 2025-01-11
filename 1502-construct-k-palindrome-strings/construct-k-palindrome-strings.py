78d7b7cd4ee35c989758380e6f0cc6bcead31a13
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-01-11 19:05:19 +0530

    Time: 23 ms (98.24%) | Memory: 18.1 MB (16.30%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -1,11 +1,13 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-
-        freq = Counter(s)
-        
-        odd_count = sum(1 for count in freq.values() if count % 2 != 0)
-       
-        return odd_count <= k <= len(s)
-
-
-        
+        if len(s) == k:
+            return True
+        if len(s) < k:
+            return False
+        odd = 0
+        for char in set(s):
+            if s.count(char) % 2:
+                odd += 1
+        if odd > k:
+            return False
+        return True

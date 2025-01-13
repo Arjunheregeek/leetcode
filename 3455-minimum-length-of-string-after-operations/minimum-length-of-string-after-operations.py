c87f657637d8df1258676aeda549a61be5c9aef9
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-01-13 16:40:50 +0530

    Time: 63 ms (92.06%) | Memory: 18.6 MB (61.90%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,11 +1,12 @@
 class Solution:
-    def minimumLength(self, s: str) -> int:
-        freq = Counter(s)
-        cnt = 0
-        for key in freq :
-            if freq[key]%2 == 0 :
-                cnt+= 2 
-            else :
-                cnt+=1 
 
-        return cnt
+    def minimumLength(self, s: str) -> int:
+        s_set = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+        ans = 0
+        for ch in s_set:
+            count = s.count(ch)
+            if count & 1:
+                ans += 1
+            elif count != 0:
+                ans += 2
+        return ans

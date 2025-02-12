d5d2daaf4a5c9058916a4a1293720842c74bcc3f
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-02-12 20:28:21 +0530

    Time: 324 ms (63.13%) | Memory: 33.6 MB (68.69%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,29 +1,24 @@
-import numpy as np
 class Solution:
-    import numpy as np
+    def sum_digit(self,x):
+        ds = 0 
+        while x>0 :
+            ds += x%10 
+            x //= 10
+
+        return ds
+
+
     def maximumSum(self, nums: List[int]) -> int:
-        ans=0
-        d={}
-        for i in  nums:
-            
-            
-            k=i
-            j = 0
-            while k> 0:
-                j+= k % 10  # Get last digit and add to total
-                k //= 10
-            if j in d :
-                ans=max(ans,i+d[j])
-            else:
-                d[j]=i
+        dic = {}
+        maxi = -1
+        for i in nums :
+            x = self.sum_digit(i)
 
-            d[j]=max(d[j],i)
-        if ans==0:
-            return -1
-        return ans
-            
-                   
+            if x in dic :
+                maxi=max(maxi,i+dic[x])
+            else :
+                dic[x]=i
+            dic[x]=max(dic[x],i)
 
-
-
-        
+                
+        return maxi

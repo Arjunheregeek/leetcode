df18d089f780b86d892846f4c9f7bec7ea971def
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-02-23 20:05:22 +0530

    Time: 7 ms (9.15%) | Memory: 18 MB (28.31%) - LeetSync

---
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,20 @@
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        n  = len(postorder)
+        postvalind = {} 
+        for i , num in enumerate (postorder) :
+            postvalind[num] = i 
+
+        def build(i1, i2, j ) :
+            if i1 > i2 :
+                return None 
+            
+            root = TreeNode( preorder [ i1 ] ) 
+            if i1 != i2 :
+                left_val = preorder[ i1 + 1 ] 
+                mid = postvalind[left_val] 
+                leftsize = mid - j + 1
+                root.left = build (i1 + 1 , i1+ leftsize , j )
+                root.right = build (i1 + leftsize + 1 , i2 , mid+1 )
+            return root  
+        return build (0 , n-1, 0 )

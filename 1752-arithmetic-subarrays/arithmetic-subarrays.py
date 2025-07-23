7fd73460eb530aa75899342bebece895f6e6797f
Arjun Gupta <148304957+Arjunheregeek@users.noreply.github.com>
2025-07-23 14:30:55 +0530

    Time: 38 ms (58.18%) | Memory: 18.1 MB (12.60%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -5,17 +5,14 @@ class Solution:
             sa=nums[l[i]:r[i]+1]
             sa.sort()
             d=sa[0]-sa[1]
-            p=0
+            p=True
             for j in range(1,len(sa)-1):
                 if sa[j]-sa[j+1]==d:
                     continue 
                 else:
-                    p=1
+                    p=False
                     break
-            if p==0:
-                ans.append(True)
-            else:
-                ans.append(False)
+            ans.append(p)
         return ans
                 
 
